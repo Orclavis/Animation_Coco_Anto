ff7f052dd639877749ea1dd4faa27d91efb0702f
unknown <usrlocal@GA211-FE14.ad.univ-angers.fr>
2017-12-06 11:50:40 +0100

    Grab Controller and vibration

---
+++ Assets/Script/ControllerGrabObject.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private GameObject collidingObject;
+
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        if (GetComponent<FixedJoint>())
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHand = null;
+    }
+
+    IEnumerator LongVibration(float length, ushort strength)
+    {
+        for (float i = 0; i < length; i += Time.deltaTime)
+        {
+            Controller.TriggerHapticPulse(strength);
+            yield return null; //every single frame for the duration of "length" you will vibrate at "strength" amount
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+         if (Controller.GetHairTriggerDown())
+         {
+            if (collidingObject)
+            {
+                StartCoroutine (LongVibration(0.1f , 3999));
+                GrabObject();
+            }
+         }
+
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+
+}
