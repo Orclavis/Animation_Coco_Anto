6cbfef7d8063571515a5fb6ae26a618129f7feed
Corentin Turcaud <corentin.turcaud@gmail.com>
2017-12-06 12:19:45 +0100

    Beginning door animation

--- Assets/Models/Orc/OrcAnimation.cs
+++ Assets/Models/Orc/OrcAnimation.cs
@@ -6,11 +6,13 @@ public class OrcAnimation : MonoBehaviour {
     Animator animator;
     Animation idle;
     GameObject FBX;
+    GameObject porte;
     bool isWalking;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         FBX = GameObject.Find("FBX");
+        porte = GameObject.Find("fi_vil_door04");
 	}
 	
 	// Update is called once per frame
@@ -31,4 +33,11 @@ public class OrcAnimation : MonoBehaviour {
             FBX.transform.Translate(new Vector3(0, 0, 0.01f));
         }
     }
+
+    IEnumerator Entree()
+    {
+        
+        yield return new WaitForSeconds(2);
+    }
+
 }
