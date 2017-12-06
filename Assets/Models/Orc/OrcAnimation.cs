72e8ec9c57081180afbb9eab4ea5469985c2b52f
Corentin Turcaud <corentin.turcaud@gmail.com>
2017-12-06 11:23:48 +0100

    Add orc animation

---
+++ Assets/Models/Orc/OrcAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrcAnimation : MonoBehaviour {
+    Animator animator;
+    Animation idle;
+    GameObject FBX;
+    bool isWalking;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        FBX = GameObject.Find("FBX");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetFloat("Speed", 2.0f);
+            isWalking = true;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetFloat("Speed", 0.0f);
+            isWalking = false;
+        }
+        if (isWalking)
+        {
+            FBX.transform.Translate(new Vector3(0, 0, 0.01f));
+        }
+    }
+}
