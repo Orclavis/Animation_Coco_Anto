553e0d41931213cad70b6e1537bda21b0d9b5910
Corentin Turcaud <corentin.turcaud@gmail.com>
2017-12-18 16:04:27 +0100

    Begin animation after 10 sec

--- Assets/Models/Orc/OrcAnimation.cs
+++ Assets/Models/Orc/OrcAnimation.cs
@@ -8,25 +8,30 @@ public class OrcAnimation : MonoBehaviour {
     Animation idle;
     GameObject FBX;
     GameObject porte;
+    AudioSource audioSource;
     bool isWalking;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         FBX = GameObject.Find("FBX");
         porte = GameObject.Find("fi_vil_door04");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Sequence EntreeOrc = DOTween.Sequence();
-            EntreeOrc.Append(porte.transform.DORotate(new Vector3(0, -90, 0), 3, RotateMode.LocalAxisAdd).SetLoops(2, LoopType.Yoyo));
-            EntreeOrc.Insert(1, (gameObject.transform.DOMoveZ(-6, 4)).
-                OnComplete(() => animator.SetBool("IsWalking", false))).
-                OnStart(() => animator.SetBool("IsWalking", true));
-
-        }
+        audioSource = gameObject.GetComponent<AudioSource>();
+        StartCoroutine(startAnim());
     }
+	
 
+    private IEnumerator startAnim()
+    {
+        yield return new WaitForSeconds(10);
+        Sequence EntreeOrc = DOTween.Sequence();
+        EntreeOrc.Append(porte.transform.DORotate(new Vector3(0, -90, 0), 3, RotateMode.LocalAxisAdd).SetLoops(2, LoopType.Yoyo));
+        EntreeOrc.Insert(1, (gameObject.transform.DOMoveZ(-6, 4)).
+            OnComplete(() => animator.SetBool("IsWalking", false))).
+            OnStart(() => startWalking()).SetDelay(3);
+    }
+    private void startWalking()
+    {
+        animator.SetBool("IsWalking", true);
+        audioSource.Play();
+    }
 }
