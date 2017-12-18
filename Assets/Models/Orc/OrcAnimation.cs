cb93175a1ae7f313dba9ff52ddbbf601d255637a
Corentin Turcaud <corentin.turcaud@gmail.com>
2017-12-18 17:32:52 +0100

    Finish Orc Animations

--- Assets/Models/Orc/OrcAnimation.cs
+++ Assets/Models/Orc/OrcAnimation.cs
@@ -18,11 +18,19 @@ public class OrcAnimation : MonoBehaviour {
         audioSource = gameObject.GetComponent<AudioSource>();
         StartCoroutine(startAnim());
     }
-	
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            animator.SetBool("IsDancing", true);
+            //animator.SetBool("IsDancing", false);
+            StartCoroutine(GiveSword());
+        }
+    }
 
     private IEnumerator startAnim()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(1);
         Sequence EntreeOrc = DOTween.Sequence();
         EntreeOrc.Append(porte.transform.DORotate(new Vector3(0, -90, 0), 3, RotateMode.LocalAxisAdd).SetLoops(2, LoopType.Yoyo));
         EntreeOrc.Insert(1, (gameObject.transform.DOMoveZ(-6, 4)).
@@ -34,4 +42,24 @@ public class OrcAnimation : MonoBehaviour {
         animator.SetBool("IsWalking", true);
         audioSource.Play();
     }
+
+    private IEnumerator GiveSword()
+    {
+        yield return new WaitForSeconds(7);
+        gameObject.transform.DORotate(new Vector3(0, 180, 0), 3, RotateMode.LocalAxisAdd);
+        yield return new WaitForSeconds(4);
+        Sequence SortieOrc = DOTween.Sequence();
+        SortieOrc.Append(porte.transform.DORotate(new Vector3(0, -90, 0), 3, RotateMode.LocalAxisAdd));
+        SortieOrc.Append(gameObject.transform.DOMoveZ(-2.2f, 4).
+            OnComplete(() => endWalking()).
+            OnStart(() => animator.SetBool("IsWalking", true)));
+        SortieOrc.Append(porte.transform.DORotate(new Vector3(0, 90, 0), 3, RotateMode.LocalAxisAdd));
+    }
+
+    private void endWalking()
+    {
+        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsDancing", false);
+    }
+
 }
