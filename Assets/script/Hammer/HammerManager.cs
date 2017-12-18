ff6b36cf54d0d022eba0e0cda831a91195298196
unknown <usrlocal@GA211-FE13.ad.univ-angers.fr>
2017-12-18 16:40:10 +0100

    add interaction

--- Assets/script/Hammer/HammerManager.cs
+++ Assets/script/Hammer/HammerManager.cs
@@ -5,18 +5,10 @@ namespace forge.hammer
 {
   public class HammerManager : MonoBehaviour
   {
-    
-    private Rigidbody rigidBody;
-
-    void Start()
-    {
-      rigidBody = GetComponent<Rigidbody>();
-    }
-
 
     private void OnCollisionEnter(Collision collision)
     {
-      if (collision.impulse.sqrMagnitude >= 100)
+      if (collision.impulse.sqrMagnitude >= 5)
       {
         GameObject hited=collision.gameObject;
         if (hited.tag=="Iron")
--- Assets/script/Iron/IronManager.cs
+++ Assets/script/Iron/IronManager.cs
@@ -2,22 +2,61 @@
 
 namespace forge.Iron
 {
-  public class IronManager : MonoBehaviour,IIronManager
-  {
-    public void Upgrade()
-    {
-      Debug.Log("Here");
-    }
-
-    // Use this for initialization
-    void Start()
+    public class IronManager : MonoBehaviour, IIronManager
     {
 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private int nbCoup = 0;
+        private GameObject anvil = null;
+        private Renderer rend;
+        private Color orange;
+        private GameObject sword;
+
+        void Start()
+        {
+            rend = GetComponent<Renderer>();
+            rend.material.SetColor("_Color", Color.red);
+            orange = new Color(1F, 165.0f/255.0f, 0F);
+            sword = GameObject.Find("sword");
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Upgrade();
+        }
+
+        public void Upgrade()
+        {
+            if (anvil)
+            {
+                Debug.Log(nbCoup);
+                if (nbCoup == 0)
+                    rend.material.SetColor("_Color", orange);
+                if (nbCoup == 1)
+                    rend.material.SetColor("_Color",Color.yellow);
+                if (nbCoup == 2)
+                {
+                    sword.transform.position = transform.position+new Vector3(0.8f,0,0);
+                    Destroy(gameObject);
+                }
+                    
+                
+                nbCoup++;
+
+            }
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (!anvil && collision.gameObject.tag == "Anvil")
+                anvil = collision.gameObject;
+        }
+
+        public void OnCollisionExit(Collision collision)
+        {
+            if (anvil && collision.gameObject.tag == "Anvil")
+                anvil = null;
+        }
     }
-  }
 }
